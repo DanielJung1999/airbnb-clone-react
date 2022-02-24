027f46d1f4fa56b7806a022c200fe88fe436c49a
Dániel Jung <danielzsoltjung@gmail.com>
2022-02-24 17:20:30 +0100

    Added a reusable Card component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import './index.css';
 import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero";
+import Card from "./Components/Card";
+import KatiePicture from './Images/Katie.jpg'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Card
+        img = {KatiePicture}
+        rating = "5.0"
+        reviewCount = {6}
+        country = "USA"
+        title = "Life lessons with Katie Zaferes"
+        price = {136}
+      />
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import '../index.css';
+import StarImg from '../Images/star.png'
+
+export default function Card(props){
+    return(
+        <div className='card'>
+            <img src={props.img} className='card--img'></img>
+            <div className='card--stats'>
+                <img src={StarImg} className='card--star'></img>
+                <span>{props.rating} </span>
+                <span className='gray'>({props.reviewCount}) - </span>
+                <span className='gray'>{props.country}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span className='bold'>From ${props.price}</span> / person</p>
+        </div>
+    )
+}
